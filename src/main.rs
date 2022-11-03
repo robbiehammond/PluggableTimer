e3e8716155bd5c51fcd943a4c59e1365d37dba4f
Robbie Hammond <robbiehammond3@gmail.com>
2022-11-02 20:44:13 -0400

    prints stuff multiple times

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::os::unix::net::UnixListener;
 use std::os::unix::net::UnixStream;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use std::thread;
 
 struct Timer {
     client: i32, //The ID of the client who owns this timer 
@@ -13,16 +14,18 @@ struct Timer {
 fn main() -> std::io::Result<()> {
     let mut client_timers: HashMap<i32, Timer> = HashMap::new();
     let listener = UnixListener::bind("/tmp/pluggable_timer.sock")?;
-    loop {
-        match listener.accept() {
-            Ok((mut socket, addr)) => {
-                println!("Got a client: {:?}", addr);
-                let mut buf = [0; 1024];
-                let count = socket.read(&mut buf).unwrap();
-                let res = String::from_utf8(buf[..count].to_vec()).unwrap();
-                println!("{}", res);
-            },
-        Err(e) => println!("accept function failed: {:?}", e),
+    Ok(for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                thread::spawn(move || {
+                    let mut buf = [0; 1024];
+                    let count = stream.read(&mut buf).unwrap();
+                    let res = String::from_utf8(buf[..count].to_vec()).unwrap();
+                    println!("{}", res);
+                });
+            }
+            Err(e) => println!("damn..."),
         }
-    }
+    })
+
 }
--- src/test.py
+++ src/test.py
@@ -6,8 +6,7 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
     client.connect("/tmp/pluggable_timer.sock")
     i = 0
     while i < 10:
-        client.send(b"Client 1: hi\n")
-        time.sleep(1)
+        client.send(f'{i}\n'.encode())
         i += 1
 
     client.close()
